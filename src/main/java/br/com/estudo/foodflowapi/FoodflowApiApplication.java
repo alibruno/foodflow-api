8001c69084a429d7bcd0aad2505f1a36d17101c4
Alisson Bruno <alissonbruno318@gmail.com>
2026-06-19 17:18:04 -0300

    inital commit

---
+++ src/main/java/br/com/estudo/foodflowapi/FoodflowApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.foodflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodflowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/estudo/foodflowapi/FoodflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.foodflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
